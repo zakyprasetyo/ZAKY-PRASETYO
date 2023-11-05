357ed8c7e3a599bc3e36a535177e09ada302057a
zakyprasetyo <149952985+zakyprasetyo@users.noreply.github.com>
2023-11-05 22:14:28 +0700

    Add files via upload

---
+++ TUGAS.m
@@ -0,0 +1,85 @@
+%NAMA: M. ZAKY PRASETYO 
+%NIM : 112021143
+A= [ 1 2 3; 2 1 1; 3 2 1]
+B= [ 4 4 5; 6 1 2; 3 5 5]
+
+det (A)
+det (B)
+
+size (A)
+size (B)
+
+trace (A)
+trace (B)
+
+norm (A)
+norm (B)
+
+C = A + B
+
+C = A - B
+
+C = A * B
+
+C = A.*B
+
+C = A^2
+
+C = A.^2
+
+T = A'
+T = B'
+
+C = A./B
+
+C = A.\B
+
+C = A/B
+
+inv (A)
+inv (B)
+
+C = null (A)
+
+C = orth (A)
+
+C = rref (A)
+
+C = eig (A)
+C = eig (B)
+
+C = svd (A)
+C = svd (B)
+
+S = triu (A)
+S = triu (B)
+
+S = tril (A)
+S = tril (B)
+
+S = max (A)
+S = max (B)
+
+S = min (A)
+S = min (B)
+
+S = sum (A)
+S = sum (B)
+
+S = diag (A)
+S = diag (B)
+
+
+I = eye (5,5)
+
+I = zeros (3,3)
+
+I = ones (4,4)
+
+I = rand (3,4)
+
+I = randn (3,4)
+
+
+
+
